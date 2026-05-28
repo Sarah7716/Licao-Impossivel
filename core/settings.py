10f5e4c3f529ff0306ed0065e7c52a8ddf24981b
Nicollas Santos <nicollassantos660@gmail.com>
2026-05-28 11:31:21 -0300

    termina modelo user profile

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'userprofile',
+    'product'
 ]
 
 MIDDLEWARE = [
@@ -115,3 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Configuração do pillow
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
