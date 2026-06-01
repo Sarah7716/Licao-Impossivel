f99a4631cc86b64b817ccdf0c1504d705e2961ba
Nicollas Santos <nicollassantos660@gmail.com>
2026-06-01 12:09:08 -0300

    add routers and views

---
+++ product/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('register/', views.product_register, 
+    name='product_register')
+]
--- userprofile/models.py
+++ userprofile/models.py
@@ -3,13 +3,9 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class UserPrfile(models.Model):
-    user = models.OneToOneField(User,
-    on_delete=models.CASCADE, blank=False,
-    null=False,
-    related_name='userprofile_user_set')
-    picture = models.ImageField(upload_to='userprofile/',
-    blank=True, null=True)
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=False, null=False, related_name='userprofile_user_set')
+    picture = models.ImageField(upload_to='userprofile/', blank=True, null=True)
     birthdate = models.DateField(blank=True, null=True)
     created_at = models.DateField(auto_now_add=True)
 
