e711ce5993e80b45c4e9fc1365eda95cd1fd8c69
Sarah7716 <sarahgoncalvesoliveira777@gmail.com>
2026-06-19 08:39:12 -0300

    testte de commit

--- userprofile/views.py
+++ userprofile/views.py
@@ -1,6 +1,9 @@
+import json
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.contrib.auth.models import User
+from .models import UserProfile
 
 # Create your views here.
 def home_view(request):
@@ -12,4 +15,30 @@ def userprofile_view(request):
 @csrf_exempt
 def  userprofile_register(request):
     if request.method == 'POST':
-        pass
+        # 1. Transforma o JSON do Bruno em um dicionário python
+        data = json.loads(request.body)
+         
+        # 2. Cria o User padrão do Django (criptografando a senha)
+        new_user = User.objects.create_user(
+            username = data['username'],
+            password = data['password'],
+            email = data['email']
+        )
+        # 3. Cria o UserProfile vinculando ao User criando acima
+        new_user_profile = UserProfile.objects.create(
+            user = new_user, 
+            birthdate = data['birthdate']
+        )
+
+        return JsonResponse(
+            {
+                'status': 'Sucesso',
+                'message': 'Usuário criado!',
+            },status=201
+        ) 
+
+    return JsonResponse(
+            {
+                'erro': 'Método não pertmitido'
+            },status=405
+    )
